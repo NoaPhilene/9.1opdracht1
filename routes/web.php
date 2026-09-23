7aa3fc15b7eed1b74cc5abd44d1404cc99963e9f
NoaPhilene <philenevanmil@gmail.com>
2026-09-23 12:02:41 +0200

    html omgezt laravel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/post/{id}', function ($id) {
+    return view('post');
+})->name('post.show');
